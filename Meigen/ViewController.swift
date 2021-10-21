92cd5a2c14651a6a79def73fa4bf5f1362552eac
tomobou5103 <local3939.info@gmail.com>
2021-10-21 11:51:36 +0900

    Initial Commit

---
+++ Meigen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meigen
+//
+//  Created by TomoHarada on 2021/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
