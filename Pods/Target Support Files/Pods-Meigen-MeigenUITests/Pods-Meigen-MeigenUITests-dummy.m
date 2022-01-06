f503234a692ee6f644f8501127ed1c1ddabdf16a
tomobou5103 <local3939.info@gmail.com>
2022-01-06 16:03:49 +0900

    initial commit

---
+++ Pods/Target Support Files/Pods-Meigen-MeigenUITests/Pods-Meigen-MeigenUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Meigen_MeigenUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Meigen_MeigenUITests
+@end
---
+++ Pods/Target Support Files/Pods-MeigenTests/Pods-MeigenTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeigenTests : NSObject
+@end
+@implementation PodsDummy_Pods_MeigenTests
+@end
