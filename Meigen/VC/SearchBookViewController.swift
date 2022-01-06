5ae904d8565aaefe4b86791fe0eaafc1efde8ca7
tomobou5103 <local3939.info@gmail.com>
2022-01-06 16:36:41 +0900

    create SearchBookViewController

---
+++ Meigen/VC/SearchBookViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class SearchBookViewController: UIViewController {
+
+    @IBOutlet weak private var textF: UITextField!
+    @IBOutlet weak private var tableV: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   }
+}
