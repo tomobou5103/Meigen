fc7433d510f5df5c975df844b4eabbd82d9fc414
tomobou5103 <local3939.info@gmail.com>
2022-02-03 01:56:06 +0900

    add function of select picture related Meigen in RegisterViewController

--- Meigen/VC/RegistViewController.swift
+++ Meigen/VC/RegistViewController.swift
@@ -1,34 +1,35 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Photos
 
 
 final class RegistViewController: UIViewController {
 
-//MARK: Property-
+//MARK: -Property
     private var model:Item?
-//MARK: IBOutlet-
+//MARK: -IBOutlet
     @IBOutlet weak private var MeigenTextView: UITextView!
     @IBOutlet weak private var barButton: UIBarButtonItem!
     @IBOutlet weak private var bookNameTextField: UITextField!
     @IBOutlet weak private var authorTextField: UITextField!
     @IBOutlet weak private var commentTextField: UITextField!
-//MARK: IBAction-
+    @IBOutlet weak private var imageV: UIImageView!{didSet{imageV.alpha = 0}}
+//MARK: -IBAction
     
     @IBAction private func insertImageButton(_ sender: Any) {
+        checkPermission()
+        selectImage()
     }
     
     @IBAction private func barButtonAction(_ sender: Any) {
         //UD()
-        
-        
-        
-        
         //rx isHidden
     }
     internal func configure(model:Item){
         self.model = model
     }
+//MARK: -LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let model = self.model else{
@@ -36,5 +37,49 @@ final class RegistViewController: UIViewController {
         }
         self.bookNameTextField.text = model.volumeInfo.title
         self.authorTextField.text = model.volumeInfo.authors?[0]
+    }
+}
+//MARK: -Extension
+extension RegistViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func selectImage(){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let imagePicker = UIImagePickerController.init()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func checkPermission(){
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("auth")
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({(newStatus) in
+                print("status is \(newStatus)")
+                if newStatus == PHAuthorizationStatus.authorized{
+                    print("Success")
+                }
+            })
+            print("not Determind")
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        case .limited:
+            print("limited")
+        default:
+            print("default")
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if info[UIImagePickerController.InfoKey.originalImage] != nil {
+            let image = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+            imageV.alpha = 1
+            imageV.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
