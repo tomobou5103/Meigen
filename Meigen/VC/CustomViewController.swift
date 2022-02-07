cba90f7d9353d20ce7925200f2b4f915d645e03f
tomobou5103 <local3939.info@gmail.com>
2022-02-08 04:04:51 +0900

    add CustomTableViewCell and TableView

--- Meigen/VC/CustomViewController.swift
+++ Meigen/VC/CustomViewController.swift
@@ -1,7 +1,25 @@
 import UIKit
-class CustomViewController: UIViewController {
-
+final class CustomViewController: UIViewController {
+    
+//MARK: -IBOutlet
+    @IBOutlet weak var tableV: UITableView!{
+        didSet{
+            tableV.delegate = self
+            tableV.dataSource = self
+        }
+    }
+//MARK: -LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+//MARK: -Extension
+extension CustomViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
     }
 }
