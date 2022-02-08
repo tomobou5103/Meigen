e08caa9e57c5de508bf50f6cba5f2516324d49ea
tomobou5103 <local3939.info@gmail.com>
2022-02-08 10:59:13 +0900

    add RegistBookModel

--- Meigen/Utility/RegistBookModel.swift
+++ Meigen/Utility/RegistBookModel.swift
@@ -1,5 +1,27 @@
-import Foundation
+import UIKit
 
 struct CategoryModel{
+    internal var title:String = ""
+    internal var author:[String?]
+    internal var comment:String?
+    internal var meigenText:String?
+    internal var bookImage:UIImage?
+    internal var meigenImage:UIImage?
     
+    init(
+        title:String,
+        _ author:[String?],
+        _ comment:String?,
+        _ meigenText:String?,
+        _ bookImage:UIImage?,
+        _ meigenImage:UIImage?
+    ){
+        self.title = title
+        self.author = author
+        self.comment = comment
+        self.meigenText = meigenText
+        self.bookImage = bookImage
+        self.meigenImage = meigenImage
+    }
 }
+ 
