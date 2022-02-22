9837ef9e1fd33dc8a9de6befb6f9bef8e4e78bfb
tomobou5103 <local3939.info@gmail.com>
2022-02-22 16:35:49 +0900

    add MenuVC and MenuView

---
+++ Meigen/VC/MenuViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+class MenuViewController: UIViewController {
+    @IBOutlet weak var menuView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let menuPos = self.menuView.layer.position
+        self.menuView.layer.position.x = -self.menuView.frame.width
+        UIView.animate(
+            withDuration: 0.2,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: {
+                self.menuView.layer.position.x = menuPos.x
+        },
+            completion: { bool in
+        })
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches {
+            if touch.view?.tag == 1 {
+                UIView.animate(
+                    withDuration: 0.1,
+                    delay: 0,
+                    options: .curveEaseIn,
+                    animations: {
+                        self.menuView.layer.position.x = -self.menuView.frame.width
+                },
+                    completion: { bool in
+                        self.dismiss(animated: true, completion: nil)
+                }
+                )
+            }
+        }
+    }
+}
