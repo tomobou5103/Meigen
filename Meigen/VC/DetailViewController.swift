a2e40b4c6f391bd95d68555e8254f470948c7c90
tomobou5103 <local3939.info@gmail.com>
2022-03-16 06:51:52 +0900

    add DetailViewController

---
+++ Meigen/VC/DetailViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
